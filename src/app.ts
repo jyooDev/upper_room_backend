469c7ff787f9bf90f64841e8792f3874a10f282e
jyooDev <gina07099@gmail.com>
2025-08-27 11:42:01 -0500

    ✨ Add cors to express

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express, { type Request, type Response } from 'express'; // import express package
+import cors from 'cors';
 
-import apiRoutes from './routes'
+import apiRoutes from './routes';
 
 const app = express(); // instantiate the express server
 
+app.use(cors()); // This enables CORS for all origins and methods
 app.use(express.json());
-app.use("/api", apiRoutes)
+app.use('/api', apiRoutes);
 
-app.get("/ping", (req: Request, res: Response) => {
-  res.send("pong from Jinah")
-})
+app.get('/ping', (req: Request, res: Response) => {
+  res.send('pong from Jinah');
+});
 
-export default app
+export default app;

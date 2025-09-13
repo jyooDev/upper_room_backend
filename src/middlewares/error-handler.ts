26a2f1591b8794a27100608c22e87e7fa8e45584
jyooDev <gina07099@gmail.com>
2025-09-13 17:11:47 -0500

    🩹 Fix errorhandler and remove logs

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,15 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 
-function errorHandler(
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
+function errorHandler(err: any, req: Request, res: Response) {
   const statusCode = err.status || 500;
   const message = err.message || 'Internal Server Error';
   res.status(statusCode).json({ message, error: err });
-  console.log('HITTED');
 }
 
 export default errorHandler;

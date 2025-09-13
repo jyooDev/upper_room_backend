fa98c6f876ef1d13630ae7c286aed61ab8e5a06b
jyooDev <gina07099@gmail.com>
2025-09-13 17:25:41 -0500

    🔇 Remove payload logs

--- src/middlewares/verify-token.ts
+++ src/middlewares/verify-token.ts
@@ -1,23 +1,26 @@
 import admin from '../firebaseAdmin';
 import { jwtDecode } from 'jwt-decode';
 import { Request, Response, NextFunction } from 'express';
+import Logger from '../utils/logger';
+
+const logger = new Logger('/src/middlewares/verify-token.ts');
 
 const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
   const header = req.headers.authorization;
-  if (!header) throw 'NO TOKEN';
+  if (!header) throw 'NO HEADER';
   // Bearer <token value>
   const idToken = header.split(' ')[1];
 
-  const payload = jwtDecode(idToken);
-  console.log(payload.aud, payload.iss, payload.exp);
+  // const payload = jwtDecode(idToken);
+  // console.log(payload.aud, payload.iss, payload.exp);
 
   try {
     const decoded = await admin.auth().verifyIdToken(idToken);
-    console.log('decoded: ', decoded);
+    logger.debug('Id Token is Valid');
     req.user = decoded;
     next();
   } catch (error) {
-    console.log('error =', error);
+    logger.error('error =', error);
     return res.status(401).send('UNAUTHORIZED');
   }
 };

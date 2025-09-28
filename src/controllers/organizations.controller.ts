8f40e49c102f42b6bfc5f2e2c6ed3e5f611f23c5
jyooDev <gina07099@gmail.com>
2025-09-27 21:46:15 -0500

    ✨ Implement organization GET API endpoint

--- src/controllers/organizations.controller.ts
+++ src/controllers/organizations.controller.ts
@@ -1,21 +1,44 @@
-class OrganizationController{
-    create(){
-        return "CREATE ORG"
+import NotFoundError from '../errors/NotFoundError';
+import Organization from '../models/organization.model';
+
+class OrganizationController {
+  create() {
+    return 'CREATE ORG';
+  }
+
+  async read(userId?: string) {
+    try {
+      if (userId) {
+        const orgs = await Organization.find().where('members').in([userId]);
+        if (!orgs) {
+          throw new NotFoundError(`No organization found for user ${userId}.`);
+        }
+        return {
+          orgs,
+          message: `Successfully fetched organizations for user ${userId}.`,
+        };
+      }
+
+      const orgs = await Organization.find();
+      if (orgs.length === 0) {
+        throw new NotFoundError(`No organization found for user ${userId}.`);
+      }
+      return {
+        orgs,
+        message: `Successfully fetched all organizations`,
+      };
+    } catch (error) {
+      throw error;
     }
+  }
 
-    read(){
-        return "LIST ORG"
-    }
+  update() {
+    return 'UPDATE ORG';
+  }
 
-
-    update(){
-        return "UPDATE ORG"
-    }
-
-    delete(){
-        return "DELETE ORG"
-    }
-
+  delete() {
+    return 'DELETE ORG';
+  }
 }
 
 export default OrganizationController;
--- src/routes/v1/organizations.routes.ts
+++ src/routes/v1/organizations.routes.ts
@@ -1,31 +1,40 @@
 import { Router } from 'express';
 
-import { OrganizationController } from "../../controllers"
-import sermonRoutes from "./sermons.routes"
+import { OrganizationController } from '../../controllers';
+import Logger from '../../utils/logger';
 
 const router = Router();
 const orgController = new OrganizationController();
+const logger = new Logger('src/routes/v1/organizations.routes.ts');
 
-router.get("/", (req, res) => {
-
-    res.send(orgController.read());
+// GET api/v1/organizations/:userId
+router.get('/{:userId}', async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    logger.debug(`GET /api/v1/organizations/${userId}`);
+    let result = null;
+    if (userId) {
+      result = await orgController.read(userId);
+    } else {
+      result = await orgController.read();
+    }
+    logger.debug(`Result: `, result);
+    res.status(201).send(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
+router.post('/', (req, res) => {
+  res.send(orgController.create());
+});
 
-router.post("/", (req, res) => {
+router.put('/:orgId', (req, res) => {
+  res.send(orgController.update());
+});
 
-    res.send(orgController.create());
-})
-
-
-router.put("/:orgId", (req, res) => {
-
-    res.send(orgController.update());
-})
-
-
-router.delete("/:orgId", (req, res) => {
-    res.send(orgController.delete());
-})
+router.delete('/:orgId', (req, res) => {
+  res.send(orgController.delete());
+});
 
 export default router;

8a66c90df87a1dae79453cca8e4af75e92304ee8
jyooDev <gina07099@gmail.com>
2025-11-05 23:02:52 -0600

    ✨ Add Livekit configuration and controller for access token generation

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,13 +5,33 @@ dotenv.config();
 interface IConfig {
   port: number;
   nodeEnv: string;
-  mongoUri: string
+  mongoUri: string;
+
+  livekitApikey: string;
+  livekitSecret: string;
+  livekitUri: string;
 }
 
 const config: IConfig = {
   port: Number(process.env.PORT) || 8888,
-  nodeEnv: process.env.NODE_ENV || "development",
-  mongoUri: (process.env.NODE_ENV === 'development' ? process.env.DEV_MONGO_URI : process.env.PROD_MONGO_URI) || "mongodb://localhost:27017/upper-room"
-}
+  nodeEnv: process.env.NODE_ENV || 'development',
+  mongoUri:
+    (process.env.NODE_ENV === 'development'
+      ? process.env.DEV_MONGO_URI
+      : process.env.PROD_MONGO_URI) || 'mongodb://localhost:27017/upper-room',
 
-export default config
+  livekitApikey:
+    (process.env.LIVEKIT_ENV === 'development'
+      ? process.env.DEV_LIVEKIT_API_KEY
+      : process.env.PROD_LIVEKIT_API_KEY) || 'devkey',
+  livekitSecret:
+    (process.env.LIVEKIT_ENV === 'development'
+      ? process.env.DEV_LIVEKIT_API_SECRET
+      : process.env.PROD_LIVEKIT_API_SECRET) || 'secret',
+  livekitUri:
+    (process.env.LIVEKIT_ENV === 'development'
+      ? process.env.DEV_LIVEKIT_URI
+      : process.env.PROD_LIVEKIT_URI) || '',
+};
+
+export default config;
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,6 @@
-export { default as UsersController } from './users.controller'
-export { default as OrganizationController } from './organizations.controller'
-export { default as PostsController } from './posts.controller'
-export { default as CommentsController } from './comments.controller'
-export { default as SermonsController } from './sermons.controller'
+export { default as UsersController } from './users.controller';
+export { default as OrganizationController } from './organizations.controller';
+export { default as PostsController } from './posts.controller';
+export { default as CommentsController } from './comments.controller';
+export { default as SermonsController } from './sermons.controller';
+export { default as LivekitController } from './livekit.controller';
